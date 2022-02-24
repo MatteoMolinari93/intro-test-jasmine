a67b4699f49dad1b0f58de6d6b6705f3e0cfa4dd
Molinari, Matteo <matteomolinari93@gmail.com>
2022-02-24 13:43:14 +0100

    More tests

--- main.js
+++ main.js
@@ -2,19 +2,40 @@
 
 
 describe(`${Person.name} Class`, () => {
+  let model;
+
+  beforeEach(() => {
+    model = new Person();
+  });
 
   describe('default values', () => {
     it('firstName defaults to empty string', () => {
-      expect(new Person({firstName: null}).firstName).toBe('');
+      expect(model.firstName).toBe('');
     });
 
     it('lastName defaults to empty string', () => {
-      expect(new Person({lastName: null}).lastName).toBe('');
+      expect(model.lastName).toBe('');
     });
 
     it('middleName defaults to empty string', () => {
-      expect(new Person({middleName: null}).middleName).toBe('');
+      expect(model.middleName).toBe('');
     });
+  });
+
+  describe('full name', () => {
+    beforeEach(() => {
+      model = new Person({firstName: 'Matteo', lastName: 'Molinari'});
+    })
+
+    it('middle initial', () => {
+      model.middleName = 'Maurizio';
+      expect(model.fullName).toBe(`${model.firstName} ${model.middleName[0]}. ${model.lastName}`);
+    });
+
+    it('wo middle name only first and last', () => {
+      model.middleName = '';
+      expect(model.fullName).toBe(`${model.firstName} ${model.lastName}`);
+    })
   })
 
 })
